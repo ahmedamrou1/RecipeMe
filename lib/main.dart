b8dae3b9518406a8c572645a9f4a205c96dab475
Ahmed <ahmedamrou10@gmail.com>
2025-09-25 19:31:07 -0400

    updated import

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:recipeme/splash_screen.dart';
 import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   print('Firebase initialized');
-  runApp(const MyApp());
+  runApp(const RecipeMeApp());
 }
 
 class RecipeMeApp extends StatelessWidget {
